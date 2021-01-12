6d7481a93a3826b94ea70d3a61adeb31b681f36d
AlveeM <alvee.akand@outlook.com>
2021-01-12 17:59:50 -0500

    added async-await exercise

---
+++ callbacks-promises/04-async-await.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        reject("Numbers must be positive");
+      }
+      resolve(a + b);
+    }, 1000);
+  });
+};
+
+const doWork = async () => {
+  const sum = await add(1, 2);
+  const sum2 = await add(sum, 3);
+  const sum3 = await add(sum2, -5);
+  return sum3;
+};
+
+doWork().then(console.log).catch(console.log);
