bca09c4d2833b3173b5f2e874d1c279bfe2d24d2
AlveeM <alvee.akand@outlook.com>
2021-01-22 16:19:09 -0500

    added authentication to Task app

--- apps/task-manager/src/routers/task.js
+++ apps/task-manager/src/routers/task.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const Task = require("../models/task");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
-router.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
+router.post("/tasks", auth, async (req, res) => {
+  // const task = new Task(req.body);
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  });
 
   try {
     await task.save();
@@ -29,36 +34,39 @@ router.get("/tasks", async (req, res) => {
   } catch (err) {
     res.status(500).send();
   }
-
-  // Task.find({})
-  //   .then((tasks) => {
-  //     if (!tasks) return res.status(404).send();
-  //     res.send(tasks);
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).send();
-  //   });
 });
 
-router.get("/tasks/:id", async (req, res) => {
+// router.get("/tasks", async (req, res) => {
+//   try {
+//     const tasks = await Task.find({});
+//     res.send(tasks);
+//   } catch (err) {
+//     res.status(500).send();
+//   }
+
+//   // Task.find({})
+//   //   .then((tasks) => {
+//   //     if (!tasks) return res.status(404).send();
+//   //     res.send(tasks);
+//   //   })
+//   //   .catch((err) => {
+//   //     res.status(500).send();
+//   //   });
+// });
+
+router.get("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findById(_id);
+    const task = await Task.findOne({
+      _id,
+      owner: req.user._id,
+    });
     if (!task) return res.status(404).send();
     res.send(task);
   } catch (err) {
     res.status(500).send();
   }
-
-  // Task.findById(_id)
-  //   .then((task) => {
-  //     if (!task) return res.status(404).send();
-  //     res.send(task);
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).send();
-  //   });
 });
 
 router.patch("/tasks/:id", async (req, res) => {
@@ -72,10 +80,14 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(id, updatedInfo, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(id);
+    updates.forEach((update) => (task[update] = updatedInfo[update]));
+    await task.save();
+
+    // const task = await Task.findByIdAndUpdate(id, updatedInfo, {
+    //   new: true,
+    //   runValidators: true,
+    // });
     if (!task) return res.status(404).send("Task not found");
     res.send(task);
   } catch (err) {
